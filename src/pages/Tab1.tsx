2a2185ab413697c8cfed44c14d8359cbf8aee83e
jcarroll-invoca <jcarroll@invoca.com>
2021-08-30 20:48:06 -0600

    Initial commit for demo project for the customer: Ionic. - Created initial dirs following tutorial @ ionic.com - installed capacitor - installed android and associated android studio (android gradle build files are committed, other android installs are -g or local installs and not in the git repo). - Did some minor changes to confirm that the `ionic serve` and `sync` and `android builds` were working correctly and that live updates were occuring.

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,22 +1,26 @@
+import React from 'react';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
+import GEOLocationContainer from '../components/GEOLocationContainer';
 import './Tab1.css';
 
 const Tab1: React.FC = () => {
+  const pageName = "Location";
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>{pageName}</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
+            <IonTitle size="large">{pageName}</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        <GEOLocationContainer />
       </IonContent>
     </IonPage>
   );
--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,22 +1,25 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab3.css';
+import React from 'react';
 
 const Tab3: React.FC = () => {
+  const pageName = "Settings";
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 3</IonTitle>
+          <IonTitle>{pageName}</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
+            <IonTitle size="large">{pageName}</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
+        <ExploreContainer name={`${pageName} Page`} />
       </IonContent>
     </IonPage>
   );
